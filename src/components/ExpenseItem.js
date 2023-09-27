6d737187022fbe3ca88377e8dfa3aabdbbb54e5f
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-09-27 15:34:13 +0800

    initial

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  const expenseData = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 284;
+
+  return (
+    <div className="expense-item">
+      <div>Date</div>
+      <div className="expense-item__description">
+        <h2>Title</h2>
+        <div className="expense-item__price">Amount</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
