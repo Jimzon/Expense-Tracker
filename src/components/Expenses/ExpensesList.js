acae0787a5dec597716fe6e36a1ec493d5483d57
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-07 21:38:28 +0800

    adding conditional return statement

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -6,6 +6,10 @@ import "./ExpensesList.css";
 const ExpensesList = (props) => {
   let expensesContent = <p>No expenses found.</p>;
 
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">Found no expenses</h2>;
+  }
+
   return (
     <ul className="expenses-list">
       {props.items.map((expense) => (
